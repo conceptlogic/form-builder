ac42e14be349b77065540757625c241cd79ebcbf
Doug Whitney <doug@conceptlogic.com>
2022-01-02 19:36:47 -0700

    Iterate on styles

--- src/App.js
+++ src/App.js
@@ -5,24 +5,63 @@ import "./App.scss";
 const App = () => {
   const [selectedElement, setSelectedElement] = useState();
 
+  const dragStart = (e) => {
+    e.target.className = `${e.target.className} isDragging`;
+  };
+
+  const dragEnd = (e) => {
+    e.target.className = e.target.className.replace("isDragging");
+  };
+
   return (
     <div className="App">
-      <header>Form Builder</header>
+      <header>
+        <h2>Form Builder</h2>
+      </header>
 
       <ul onClick={(e) => console.log(e.target.innerText)}>
-        <li className="heading" draggable title="Add a Heading">
-          <h1>Heading</h1>
+        <li title="Add a Heading">
+          <div
+            className="draggable"
+            draggable
+            onDragStart={dragStart}
+            onDragEnd={dragEnd}
+          >
+            <h1>Heading</h1>
+          </div>
         </li>
-        <li className="text-input" draggable title="Add a Text Input">
-          <input type="text" placeholder="Text Input" />
+        <li title="Add a Text Input">
+          <div
+            className="draggable"
+            draggable
+            onDragStart={dragStart}
+            onDragEnd={dragEnd}
+          >
+            <input type="text" placeholder="Text Input" />
+          </div>
         </li>
-        <li className="button" draggable title="Add a Button">
-          <button>Button</button>
+        <li title="Add a Button">
+          <div
+            className="draggable"
+            draggable
+            onDragStart={dragStart}
+            onDragEnd={dragEnd}
+          >
+            <button>Button</button>
+          </div>
         </li>
       </ul>
 
-      <div style={{ border: "dashed 2px orange", cursor: "move" }}>
-        <h2>Form Canvas</h2>
+      <h2>Form Canvas</h2>
+      <div className="formBuilder">
+        <div>Hey</div>
+        <div>Hey</div>
+        <div>Hey</div>
+        <div>Hey</div>
+        <div>Hey</div>
+        <div>Hey</div>
+        <div>Hey</div>
+        <div>Hey</div>
       </div>
     </div>
   );
