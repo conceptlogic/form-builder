a86d8ad095f1a6d128b2cbb27e896241a2897d31
Doug Whitney <doug@conceptlogic.com>
2022-01-03 08:56:53 -0700

    Setup drag/drop data transfer

--- src/ElementTray.js
+++ src/ElementTray.js
@@ -1,22 +1,28 @@
 import s from "./ElementTray.module.scss";
 
+const handlers = {
+  dragStart: (e) => {
+    e.dataTransfer.setData("text/json", e.target.dataset.element);
+  },
+};
+
 const ElementTray = () => {
   return (
-    <ul className={s.elementTray}>
+    <ul onDragStart={handlers.dragStart} className={s.elementTray}>
       <li title="Add a Heading">
-        <div className={s.draggable} draggable>
+        <div className={s.draggable} draggable data-element="heading">
           <h1>Heading</h1>
         </div>
       </li>
 
       <li title="Add a Text Input">
-        <div className={s.draggable} draggable>
+        <div className={s.draggable} draggable data-element="text-input">
           <input type="text" placeholder="Text Input" />
         </div>
       </li>
 
       <li title="Add a Button">
-        <div className={s.draggable} draggable>
+        <div className={s.draggable} draggable data-element="button">
           <button>Button</button>
         </div>
       </li>
--- src/FormCanvas.js
+++ src/FormCanvas.js
@@ -1,27 +1,41 @@
+import { useState } from "react";
+
 import s from "./FormCanvas.module.scss";
 
-const handlers = {
-  // see https://stackoverflow.com/a/50233827
-  dragOver: (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-  },
-  dragEnter: (e) => {
-    if (e.target.className !== s.formCanvas) {
-      e.target.className = `${e.target.className} ${s.dropTarget}`;
-    }
-  },
-  dragLeave: (e) => {
-    if (e.target.className !== s.formCanvas) {
-      e.target.className = e.target.className.replace(s.dropTarget, "");
-    }
-  },
-  drop: (e) => {
-    e.target.className = e.target.className.replace(s.dropTarget, "");
-  },
-};
-
 const FormCanvas = () => {
+  const [canvasData, setCanvasData] = useState(new Array(8).fill(null));
+
+  const handlers = {
+    // see https://stackoverflow.com/a/50233827
+    dragOver: (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+    },
+    dragEnter: (e) => {
+      if (e.target.className !== s.formCanvas) {
+        e.target.className = `${e.target.className} ${s.dropTarget}`;
+      }
+    },
+    dragLeave: (e) => {
+      if (e.target.className !== s.formCanvas) {
+        e.target.className = e.target.className.replace(s.dropTarget, "");
+      }
+    },
+    drop: (e) => {
+      let next = Object.assign([], canvasData, {
+        [e.target.dataset.index]: {
+          element: e.dataTransfer.getData("text/json"),
+        },
+      });
+
+      setCanvasData(next);
+
+      e.target.className = e.target.className.replace(s.dropTarget, "");
+    },
+  };
+
+  console.log(canvasData);
+
   return (
     <div
       className={s.formCanvas}
@@ -31,14 +45,11 @@ const FormCanvas = () => {
       onDrop={handlers.drop}
       title="Drop elements here"
     >
-      <div className={s.dropCell}></div>
-      <div className={s.dropCell}></div>
-      <div className={s.dropCell}></div>
-      <div className={s.dropCell}></div>
-      <div className={s.dropCell}></div>
-      <div className={s.dropCell}></div>
-      <div className={s.dropCell}></div>
-      <div className={s.dropCell}></div>
+      {canvasData.map((data, i) => (
+        <div className={s.dropCell} key={i} data-index={i}>
+          {data?.element}
+        </div>
+      ))}
     </div>
   );
 };
