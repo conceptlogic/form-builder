f3c731ecaf00eb49a8d012dd73467c715053e500
Doug Whitney <doug@conceptlogic.com>
2022-01-03 08:08:15 -0700

    Break tray and canvas out into components

--- src/App.js
+++ src/App.js
@@ -1,74 +1,20 @@
-import "./App.scss";
-
-const handlers = {
-  // see https://stackoverflow.com/a/50233827
-  dragOver: (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-  },
-  dragEnter: (e) => {
-    if (e.target.className !== "formBuilder") {
-      e.target.className = `${e.target.className} dropTarget`;
-    }
-  },
-  dragLeave: (e) => {
-    if (e.target.className !== "formBuilder") {
-      e.target.className = e.target.className.replace("dropTarget", "");
-    }
-  },
-  drop: (e) => {
-    e.target.className = e.target.className.replace("dropTarget", "");
-  },
-};
+import ElementTray from "./ElementTray";
+import FormCanvas from "./FormCanvas";
 
 const App = () => {
   return (
     <div className="App">
       <header>
-        <h2>Form Elements</h2>
+        <h1>Form Elements</h1>
         <p>
           Drag elements on to the form canvas to build and customize your form.
         </p>
       </header>
 
-      <ul onDragStart={handlers.dragStart} onDragEnd={handlers.dragEnd}>
-        <li title="Add a Heading">
-          <div className="draggable" draggable>
-            <h1>Heading</h1>
-          </div>
-        </li>
+      <ElementTray />
 
-        <li title="Add a Text Input">
-          <div className="draggable" draggable>
-            <input type="text" placeholder="Text Input" />
-          </div>
-        </li>
-
-        <li title="Add a Button">
-          <div className="draggable" draggable>
-            <button>Button</button>
-          </div>
-        </li>
-      </ul>
-
-      <h2>Form Canvas</h2>
-      <div
-        className="formBuilder"
-        onDragOver={handlers.dragOver}
-        onDragEnter={handlers.dragEnter}
-        onDragLeave={handlers.dragLeave}
-        onDrop={handlers.drop}
-        title="Drop elements here"
-      >
-        <div className="dropCell"></div>
-        <div className="dropCell"></div>
-        <div className="dropCell"></div>
-        <div className="dropCell"></div>
-        <div className="dropCell"></div>
-        <div className="dropCell"></div>
-        <div className="dropCell"></div>
-        <div className="dropCell"></div>
-      </div>
+      <h1>Form Canvas</h1>
+      <FormCanvas />
     </div>
   );
 };
