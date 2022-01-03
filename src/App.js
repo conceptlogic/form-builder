7cd3d3f14c87af8f29fa9143f6e89f145d80e9d5
Doug Whitney <doug@conceptlogic.com>
2022-01-02 17:41:27 -0700

    CRA and element tray

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import "./App.scss";
+
+const App = () => {
+  const [selectedElement, setSelectedElement] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Form Builder</header>
+
+      <ul onClick={(e) => console.log(e.target.innerText)}>
+        <li className="heading" draggable title="Add a Heading">
+          <h1>Heading</h1>
+        </li>
+        <li className="text-input" draggable title="Add a Text Input">
+          <input type="text" placeholder="Text Input" />
+        </li>
+        <li className="button" draggable title="Add a Button">
+          <button>Button</button>
+        </li>
+      </ul>
+
+      <div style={{ border: "dashed 2px orange", cursor: "move" }}>
+        <h2>Form Canvas</h2>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
